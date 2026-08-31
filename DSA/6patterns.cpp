570932a1b07eade9355b3a11c45e4e223d0b0c83
shwet3728-ai <shwet3728@gmail.com>
2026-08-31 11:41:26 +0530

    updated

--- DSA/6patterns.cpp
+++ DSA/6patterns.cpp
@@ -6,7 +6,6 @@ using namespace std;
 // ****
 // ****
 
-
 // void pattern (int n )
 // {
 //     for (int i=0;i<n;i++)
@@ -55,7 +54,6 @@ using namespace std;
 //     }
 // }
 
-
 // 1
 // 22
 // 333
@@ -73,7 +71,6 @@ using namespace std;
 //     }
 // }
 
-
 // *****
 // ****
 // ***
@@ -85,14 +82,14 @@ using namespace std;
 //     {
 //         // for (int j =5;j>=i;j--)
 //         for (int j =0;j<n-i+1;j++)
-    
+
 //     {
 //         cout<<"*";
 //     }
 //     cout<<endl;
-    
+
 //     }
-    
+
 // }
 
 // 12345
@@ -108,44 +105,94 @@ using namespace std;
 //         {
 //         cout<<j;
 //     }
-        
+
 //     cout<<endl;
 //     }
-    
-    
+
 // }
 
-void pattern (int n )
-{
-    for (int i=0;i<n;i++)
-    {
-        // space
-        for (int j = 0; j<n-i+1;j++)
-        {
-            cout<<" " ;
-        }
-        // star
-        for (int j =0; j< 2*i+1;j++)
-        {
-            cout<<"*";
-        }
-        // space
-        for (int j =0;j<n-i+1;j++){
-            cout<<" ";
-        }
-        cout<<endl;
+// void pattern1(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         // space
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         // star
+//         for (int j = 0; j < 2 * i + 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         // space
+//         for (int j = 0; j < n - i + 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     pattern(n);
+// }
 
+// void pattern(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         for (int j = 0; j < 2 * (n - i) - 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     pattern1(n);
+//     pattern(n);
+// }
+void pattern1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    
-    
-} 
+}
+void pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
-    int n ;
-    cin >>n;
+    int n;
+    cin >> n;
+    pattern1(n);
     pattern(n);
 }
-
-
-
-
